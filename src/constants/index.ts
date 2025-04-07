28bc7dc03a21a5dd6e44e3cdec5765404ec4d5e2
GASPing4ER <simecgasper@gmail.com>
2025-04-07 20:56:54 +0200

    Add Formats constant for filtering marvel data

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+import { TFormat } from "@/types/constants";
+
+export const FORMATS: TFormat[] = [
+  { value: "", label: "All" },
+  { value: "comic", label: "Comic" },
+  { value: "magazine", label: "Magazine" },
+  { value: "digital comic", label: "Digital comic" },
+];
